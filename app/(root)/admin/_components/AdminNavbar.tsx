1d34731a65dc31dbc77666e4b266a262c8b0ec83
Megatey <megatey97@gmail.com>
2024-08-07 17:22:20 +0100

    border added to admin navbar

--- app/(root)/admin/_components/AdminNavbar.tsx
+++ app/(root)/admin/_components/AdminNavbar.tsx
@@ -5,14 +5,14 @@ import React from 'react'
 
 const AdminNavbar = () => {
     return (
-        <div className='w-full h-[10vh] bg-white flex justify-between items-center px-5'>
+        <div className='w-full h-[10vh] bg-white flex justify-between items-center px-5 border-b-[2px] border-gray-300'>
             <div>
                 <p className='font-[Montserrat] font-black italic text-lg leading-[10.5px]'>Hi, Olapeju</p>
             </div>
             <div className="flex justify-start items-center gap-4">
                 <div className='relative w-fit h-fit'>
-                <NotificationIcon />
-                <Badge className='absolute top-0 -right-[8%] bg-[#e62e2e] text-white text-xs'>10</Badge>
+                    <NotificationIcon />
+                    <Badge className='absolute top-0 -right-[8%] bg-[#e62e2e] text-white text-xs'>10</Badge>
                 </div>
                 <Avatar>
                     <AvatarImage src="https://github.com/shadcn.png" />
