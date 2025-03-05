ed1a2a9553eceb774507a9c10831011496d252bb
Oladipupo Akerele <36505228+dipzcoded@users.noreply.github.com>
2025-03-05 16:11:06 +0100

    fixed

--- app/(root)/mandate/Research/coordination-report/page.tsx
+++ app/(root)/mandate/Research/coordination-report/page.tsx
@@ -21,10 +21,10 @@ export default function page() {
                 ARCN Research Institutes
               </span>{" "} */}
                 Lorem ipsum dolor sit amet consectetur. Elementum ullamcorper
-                quis etiam <br />
+                quis etiam <br className="hidden md:block" />
                 euismod quisque montes arcu risus. Magnis posuere tincidunt elit
                 ultrices tortor. Sit
-                <br />
+                <br className="hidden md:block" />
                 semper cras sed duis tortor tempor amet gravida. Rhoncus quis
                 nisl etiam tortor.
               </p>
