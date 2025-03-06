76c251d47727a1e9d88f10cc8c2189147160770b
nancyumoru <Nancy.Jacob@uridiumworks.com>
2025-03-06 01:04:00 +0100

    programs and projects fixes

--- components/ProgramsandProjects/Banner.tsx
+++ components/ProgramsandProjects/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
         <main className="bg-black w-full lg:min-h-screen h-[330px] px-4 font-montserrat lg:px-10 ">
             <div className="text-white text-center place-content-center space-y-3 py-7 lg:space-y-8 lg:py-14">
                 <h1 className="font-bold text-3xl lg:text-[65px]">PROGRAMS AND PROJECTS</h1>
-                <p className="font-normal text-[21px]">A collection of ARCN featured news and upcoming events</p>
+                <p className="font-normal text-lg md:text-[21px]">A collection of ARCN featured news and upcoming events</p>
             </div>
             <div>
                 <Image src="/Images/Mandatepage/Worker.png" alt="worker" width={1749} height={481} />
