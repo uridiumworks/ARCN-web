681a0b3fe83763500bea4ed80f3448667a3492a8
nancyumoru <Nancy.Jacob@uridiumworks.com>
2025-03-10 13:30:21 +0100

    headers resized

--- components/Impacts/Gender/Impart.tsx
+++ components/Impacts/Gender/Impart.tsx
@@ -50,21 +50,21 @@ const Impart = () => {
                         <div className="border rounded-xl w-full lg:w-[420px] space-y-3 p-4">
                             <Image src="/Images/Aboutpage/Gray.png" alt="Gray" width={64} height={64} />
                             <p className="font-bold text-base text-[rgb(76,100,195)]">FOOD SECURITY</p>
-                            <p className="font-normal text-xs text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
+                            <p className="font-normal text-sm text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
                             <button className="font-bold text-xs text-[#171717]">READ MORE</button>
                         </div>
 
                         <div className="border rounded-xl w-full lg:w-[420px] space-y-3 p-4">
                             <Image src="/Images/Aboutpage/Lime.png" alt="Lime" width={64} height={64} />
                             <p className="font-bold text-base text-[#37B7A3]">RESEARCH INITIATIVES</p>
-                            <p className="font-normal text-xs text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
+                            <p className="font-normal text-sm text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
                             <button className="font-bold text-xs text-[#171717]">READ MORE</button>
                         </div>
 
                         <div className="border rounded-xl w-full lg:w-[420px] space-y-3 p-4">
                             <Image src="/Images/Aboutpage/Pink.png" alt="Peach" width={64} height={64} />
                             <p className="font-bold text-base text-[#CA848B]">GENDER AND YOUTH</p>
-                            <p className="font-normal text-xs text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
+                            <p className="font-normal text-sm text-[#495057]">Lorem ipsum dolor sit amet consectetur. Erat pharetra dictumst nunc vestibulum in pretium.</p>
                             <button className="font-bold text-xs text-[#171717]">READ MORE</button>
                             
                         </div>
