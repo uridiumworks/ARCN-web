b06af90d98f66226fccfd9413ff13dfd774333ce
Oladipupo Akerele <36505228+dipzcoded@users.noreply.github.com>
2025-03-24 13:04:20 +0100

    fixed

--- app/(onboarding)/login/page.jsx
+++ app/(onboarding)/login/page.jsx
@@ -1,33 +1,33 @@
-"use client";
+"use client"
 
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useLoginUser } from '@/hooks/user.hook';
 import Image from 'next/image';
+import { Link } from 'lucide-react';
 
 const Login = () => {
   const { login, data, loading, error } = useLoginUser();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
 
   useEffect(() => {
     if (data?.success) {
-      console.log("Login data:", data); // Debugging
+      console.log('Login data:', data); // Debugging
       if (data.data?.token) {
-        localStorage.setItem("userToken", data.data.token);
-        router.push("/admin");
+        localStorage.setItem('userToken', data.data.token);
+        router.push('/admin');
       }
     }
   }, [data, router]);
 
   useEffect(() => {
     // Check if token exists in localStorage
-    const token = localStorage.getItem("userToken");
+    const token = localStorage.getItem('userToken');
     if (token) {
       // Redirect to admin dashboard if token exists
-      router.push("/admin");
+      router.push('/admin');
     }
   }, [router]);
 
@@ -42,26 +42,14 @@ const Login = () => {
     await login(payload);
   };
 
-  console.log(error);
-
   return (
     <div className="bg-gray-100 flex items-center justify-center h-screen">
       <div className="w-full max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
         <div className="flex justify-center mb-6">
-          <Image
-            src="/images/logo-admin.svg"
-            alt="Logo"
-            className="w-32 h-16"
-            width={32}
-            height={16}
-          />
+          <Image src="/logo.png" alt="Logo" className="w-16 h-16" width={16} height={16} />
         </div>
-        <h2 className="text-2xl font-semibold text-center text-gray-800">
-          Login
-        </h2>
-        <p className="text-center text-gray-500 mb-8">
-          Enter your credentials to login
-        </p>
+        <h2 className="text-2xl font-semibold text-center text-gray-800">Login</h2>
+        <p className="text-center text-gray-500 mb-8">Enter your credentials to login</p>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">
@@ -76,25 +64,19 @@ const Login = () => {
               className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
+
           <div className="mb-6">
             <label htmlFor="password" className="block text-gray-700">
               Password
             </label>
-            <div className="flex items-center  bg-transparent mt-2 px-4 py-2 border rounded-lg">
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full  border-none outline-none focus:outline-none focus:ring-2 focus:ring-transparent flex-1"
-              />
-              {showPassword ? (
-                <EyeOff onClick={() => setShowPassword(false)} className="cursor-pointer" />
-              ) : (
-                <Eye onClick={() => setShowPassword(true)} className="cursor-pointer" />
-              )}
-            </div>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
           </div>
 
           <button
@@ -102,7 +84,7 @@ const Login = () => {
             className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition duration-300"
             disabled={loading}
           >
-            {loading ? "Logging in..." : "Login"}
+            {loading ? 'Logging in...' : 'Login'}
           </button>
           {error && <p className="text-red-500 text-center mt-4">{error}</p>}
 
