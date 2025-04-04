7fd85ba7b69e95d9fdf3afc797ec5a3736061062
nancyumoru <Nancy.Jacob@uridiumworks.com>
2025-04-04 10:16:32 +0100

    Adjustment for Page Header:fixed

--- app/(root)/governance/_components/governance-motto.tsx
+++ app/(root)/governance/_components/governance-motto.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 
 const GovernanceMotto = () => {
     return (
-        <div className="w-full plant bg-card opacity-100 font-montserrat bg-black text-left flex flex-col place-content-center h-[19rem] px-4 md:px-72">
+        <div className="w-full plant text-black opacity-100 bg-cover bg-black/75 font-montserrat bg-black text-left flex flex-col place-content-center h-[19rem] px-4 md:px-72">
             <h1 className="text-3xl md:text-4xl font-bold text-white">
                 Governance
             </h1>
